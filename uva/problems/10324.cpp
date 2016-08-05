3a37415538712da701775d669cc70d9825b0de43
Vinícius Campos <viniciuscampos120@gmail.com>
2016-08-05 16:23:30 -0300

    Update 10324.cpp
    Update algorithm

--- uva/problems/10324.cpp
+++ uva/problems/10324.cpp
@@ -5,28 +5,28 @@ using namespace std;
 int main(){
 	
 	string zeros_ones;
+	int lista[1000000];
 	int queries, x, y;
-	int count = 0;
 	bool problem;
+	int count = 0;
 	while(cin>>zeros_ones){
+		for(int i = 0; i < zeros_ones.length(); ++i){
+			if(i == 0) 
+				lista[i] = 1;
+			else	
+				if(zeros_ones[i] == zeros_ones[i-1])
+					lista[i] = lista[i-1];
+				else
+					lista[i] = lista[i-1] + 1;
+		}
 		cout<<"Case "<<++count<<":\n";
 		cin>>queries;
 		for(int i = 0; i < queries; ++i){
 			cin>>x>>y;
-			if(x == y)
+			if(lista[x] == lista[y])
 				cout<<"Yes\n";
-			else{
-				if(y < x)
-					swap(x,y);
-				problem = false;
-				for(int j = x; j < y; ++j){
-					if(zeros_ones[j] != zeros_ones[y]){
-						problem = true;
-					}
-				}
-				if(problem) cout<<"No\n";
-				else cout<<"Yes\n";
-			}
+			else
+				cout<<"No\n";
 		}
 	}
 
